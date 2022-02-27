d1c92f341847a94a2c9300de036c4f871bae11b1
jikope <bimakope@gmail.com>
2022-02-27 12:16:12 +0700

    Add forbidden vue

---
+++ client/src/views/Forbidden.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>Anda Harus login terlebih dahulu untuk menggunakan aplikasi ini</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Forbidden",
+  metaInfo: {
+    title: "Forbidden"
+  }
+};
+</script>
